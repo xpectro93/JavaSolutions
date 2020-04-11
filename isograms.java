16eb00f910ffd32c0dc09e2942d116158e317332
xpextro93 <jonathanandrade@pursuit.org>
2020-04-11 18:38:29 -0400

    add description for isograms

---
+++ isograms.java
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/java
+/*
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that
+determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. 
+Ignore letter case.
+
+isIsogram "Dermatoglyphics" == true
+isIsogram "aba" == false
+isIsogram "moOse" == false -- ignore letter case 
+*/
+public class isograms {
+    public static boolean  isIsogram(String str) {
+       boolean answer;
+
+        return answer;
+    } 
+}
