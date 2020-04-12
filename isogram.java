cb118dd5e7a78a027ed3d4ae8d1f07e4ff71ef12
xpextro93 <jonathanandrade@pursuit.org>
2020-04-11 20:10:23 -0400

    add description of solution

--- isogram.java
+++ isogram.java
@@ -8,4 +8,29 @@ isIsogram "Dermatoglyphics" == true
 isIsogram "aba" == false
 isIsogram "moOse" == false -- ignore letter case 
 */
+import java.util.*;
 
+public class isogram {
+    public static boolean  isIsogram(String str) {
+      //turn string into array?List?
+       char[] letters = str.toCharArray();
+        
+        //create set to store unique values
+       Set<Character> seen = new HashSet<>();
+
+       //use the equivalent of a for-of loop
+       for(char letter : letters){
+
+        //turn every character to lowercase before adding to set
+        char lower = Character.toLowerCase(letter);
+
+        //Adds the specified element to this set if it is not already present.// returns boolean
+        if (!seen.add(lower)) {
+          return false;
+        }
+        
+       }
+       //if the element doesnt go through if statement we can return true, which means there were no repeated elems
+       return true;
+    } 
+}
