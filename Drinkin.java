0794d0f041f6e0e753a833f2c0094620d85b594d
xpextro93 <jonathanandrade@pursuit.org>
2020-04-13 21:13:38 -0400

    add Drinkin files

---
+++ Drinkin.java
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/5aee86c5783bb432cd000018/train/java
+
+/* 
+Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+
+Your fellow coders have bought you several drinks tonight in the form of a string.
+Return a string suggesting how many glasses of water you should drink to not be hungover.
+
+Input => "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"
+Output => "10 glasses of water"
+*/
+
+
+
+
+
+public class Drinkin {
+    public String hydrate(String drinkString) {
+        
+        return "";
+    }
+    public static void main(String[] str){
+
+    };
+}
