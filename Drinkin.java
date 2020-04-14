7e7f25b6d4fe7d1ee8aaec88ef4a8510689803d9
xpextro93 <jonathanandrade@pursuit.org>
2020-04-13 22:17:59 -0400

    add Character methods to function

--- Drinkin.java
+++ Drinkin.java
@@ -9,13 +9,21 @@ Return a string suggesting how many glasses of water you should drink to not be 
 Input => "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"
 Output => "10 glasses of water"
 */
+
 class Drinkin {
     //adding static makes it a regular function/ while not having static makes it a method to the class;
     public static String hydrate(String drinkString) {
+        int drinks = 0;
+        for(int i=0;i<drinkString.length();i++){
+            boolean current = Character.isDigit(drinkString.charAt(i));
 
-        // use regex to take out all strings that aren't numbers
-        String cleanString = drinkString.replaceAll("[^0-9]+", " ");
-        return cleanString;
+            if(current){
+               int toNumber = Character.getNumericValue(drinkString.charAt(i));
+                drinks +=toNumber;
+            } 
+            
+        }
+        return  "hello";
     }
     public static void main(String[] str){
         String test = "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer";
