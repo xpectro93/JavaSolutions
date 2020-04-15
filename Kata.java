d3e7b9f344563ae45aa541db3efea5c02ed29331
xpextro93 <jonathanandrade@pursuit.org>
2020-04-14 22:01:33 -0400

    add test

---
+++ Kata.java
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5bbd279c8f8bbd5ee500000f/train/java
+// Given an integer width and a string ratio written as WIDTH:HEIGHT, output the screen dimensions as a string written as WIDTHxHEIGHT.
+class Kata {
+    public static String findScreenHeight(int width, String ratio) {
+    	  return "Happy coding :)";
+    }
+    public static void main(String[] str){
+        System.out.println("Its working");
+    }
+}
