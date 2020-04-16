6751d8fb32dc7bf2483557ee27e3b8e013608347
xpextro93 <jonathanandrade@pursuit.org>
2020-04-15 22:59:26 -0400

    add link to problem

--- MyLanguages.java
+++ MyLanguages.java
@@ -1,4 +1,5 @@
 /*
+https://www.codewars.com/kata/5b16490986b6d336c900007d/train/java
 You are given a dictionary/hash/object containing some languages and your test results in the given languages. 
 Return the list of languages where your test score is at least 60, in descending order of the results.
 
