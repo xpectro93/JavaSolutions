b19e260f7fce9213776dbee36079228dcec95487
xpextro93 <jonathanandrade@pursuit.org>
2020-04-19 21:24:06 -0400

    add problem desc

---
+++ Troll.java
@@ -0,0 +1,9 @@
+public class Troll {
+    public static String disemvowel(String str) {
+
+        return "Floor gang";
+    }
+    public static void main(String[] str){
+        System.out.print("This is working ");
+    }
+}
