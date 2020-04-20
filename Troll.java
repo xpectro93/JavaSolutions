4b322e0d63203b11e4bac6411e9f78525004fa5a
xpextro93 <jonathanandrade@pursuit.org>
2020-04-19 21:37:32 -0400

    add attempt 1 of solution

--- Troll.java
+++ Troll.java
@@ -1,9 +1,44 @@
+/* 
+https://www.codewars.com/kata/52fba66badcd10859f00097e/train/java
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+import java.util.HashSet;
+
 public class Troll {
     public static String disemvowel(String str) {
+        HashSet<Character> vowels = new HashSet<Character>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+        
+        String out = "";
+        for (int i = 0; i < str.length(); i++){
 
-        return "Floor gang";
+            if(!vowels.contains(str.charAt(i))){
+                out+=str.charAt(i);
+            }
+        }
+        
+
+        return out;
     }
     public static void main(String[] str){
-        System.out.print("This is working ");
+        System.out.print(disemvowel("This is working"));
     }
 }
