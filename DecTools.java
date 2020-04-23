75e0192cff662b387de4ba267804a015a12e1056
xpextro93 <jonathanandrade@pursuit.org>
2020-04-22 21:39:34 -0400

    test syntax

---
+++ DecTools.java
@@ -0,0 +1,8 @@
+public class DecTools {
+    public static int Digits(long n) {
+      return 0;
+    }
+    public static void main (String[] str) {
+        System.out.println("This is wack, yo");
+    };
+  }
