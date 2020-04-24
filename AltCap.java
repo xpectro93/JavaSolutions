c97384d23a05010cbeeb2fe2d0810fa0c0cfdc0d
xpextro93 <jonathanandrade@pursuit.org>
2020-04-24 16:13:10 -0400

    add altcap file:

---
+++ AltCap.java
@@ -0,0 +1,18 @@
+/**
+ * https://www.codewars.com/kata/59cfc000aeb2844d16000075
+ * 
+ *
+ *Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+ * 
+ */
+
+class Solution{
+    public static String[] capitalize(String s){
+        // Gorillaz - Do Ya Thing (2010)
+        return new String[2];
+    }
+}
